c776572af6aaeb4ee55989ee639ef50a6c513826
Engin Arslan <hibernationtheory@gmail.com>
2015-08-11 23:52:20 -0400

    add grunt-sass plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,13 @@ var dist_path = 'dist/';
 var src_js_path = src_path + 'static/js/';
 var dist_js_path = dist_path + 'static/js/';
 
+var src_styles_path = 'src/static/styles';
+var src_styles_sass_path = 'src/static/styles/sass';
+var src_styles_css_path = 'src/static/styles/css';
+var dist_styles_path = 'dist/static/styles';
+var dist_styles_sass_path = 'dist/static/styles/sass';
+var dist_styles_css_path = 'dist/static/styles/css';
+
 var src_templates_path = src_path + 'templates/layout';
 var src_templates_pages_path = src_path + 'templates/pages/';
 var src_templates_partials_path = src_path + 'templates/partials/';
@@ -17,6 +24,14 @@ module.exports = function(grunt) {
 		'dist_path':dist_path,
 		'dist_js_path': dist_js_path,
 		'src_js_path':src_js_path,
+		/* CSS RELATED */
+		'src_styles_path':src_styles_path,
+		'src_styles_css_path':src_styles_css_path,
+		'src_styles_sass_path':src_styles_sass_path,
+		'dist_styles_path':dist_styles_path,
+		'dist_styles_sass_path':dist_styles_sass_path,
+		'dist_styles_css_path':dist_styles_css_path,
+		/* HTML RELATED */
 		'src_templates_path':src_templates_path,
 		'src_templates_pages_path':src_templates_pages_path,
 		'src_templates_partials_path':src_templates_partials_path,
@@ -30,6 +45,7 @@ module.exports = function(grunt) {
 		[
 		'assemble', /* Compile the handlebar stuff */
 		'copy', /* Copy the results over to target dir */
+		'sass', /* convert your sass into scss */
 		'browserify', /* Expand the modules using babel */
 		'replace', /* Replace target value in your files with destionation value in dist folder */
 		'watch' /* Watch for changes in a specific dir and launch a task if change happens */
